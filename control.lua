b57cf3b35d49765f712458bdb0ee064c62f7df51
ZwerOxotnik <zweroxotnik@gmail.com>
2021-12-22 20:45:25 +0300

    Update to 0.8.4

--- control.lua
+++ control.lua
@@ -1,5 +1,5 @@
 if script.active_mods["switchable_mods"] then
 	require("__switchable_mods__/event_handler_vSM").add_lib(require("models/timesaver_for_crafting"))
 else
-	require("event_handler").add_lib(require("models/timesaver_for_crafting"))
+	require("__zk-lib__/static-libs/lualibs/event_handler_vZO.lua").add_lib(require("models/timesaver_for_crafting"))
 end
